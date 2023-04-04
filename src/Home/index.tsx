4aad4305530c54994d12ce62b8577537d334d22f
fellipesnoww <fellipe2.0e@gmail.com>
2023-04-04 20:49:54 -0300

    first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './styles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
