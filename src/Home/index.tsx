150b82dde9621cf601ff689a2fd86c488afcda7f
fellipesnoww <fellipe2.0e@gmail.com>
2023-04-04 21:47:43 -0300

    apply emoji on face

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageSourcePropType, View } from 'react-native';
+import { Camera, CameraType, FaceDetectionResult } from 'expo-camera';
 import { styles } from './styles';
+import { useEffect, useState } from 'react';
+import * as FaceDetector from "expo-face-detector";
+import Animated, { useSharedValue, useAnimatedStyle } from "react-native-reanimated"
+
+import neutralImg from "../assets/neutral.png";
+import smilingImg from "../assets/smiling.png";
+import winkingImg from "../assets/winking.png";
 
 export default function Home() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [faceDetected, setFaceDetected] = useState(false);
+  const [emoji, setEmoji] = useState<ImageSourcePropType>(neutralImg);
+
+  const faceValues = useSharedValue({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0
+  })
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    position: 'absolute',
+    zIndex: 1,
+    width: faceValues.value.width,
+    height: faceValues.value.height,
+    marginTop: 250,
+    transform: [
+      { translateX: faceValues.value.x },
+      { translateY: faceValues.value.y }
+    ],
+    // borderColor: "blue",
+    // borderWidth: 10
+  }))
+
+  if(!permission?.granted) return;
+
+  function handleFacesDetected({faces}: FaceDetectionResult){
+    //console.log("Faces detected", faces);
+    const face = faces[0] as any;
+
+    if(face){
+      const { size, origin } = face.bounds;
+
+      faceValues.value = {
+        width: size.width,
+        height: size.height,
+        x: origin.x,
+        y: origin.y
+      }
+
+      setFaceDetected(true);
+      if(face.smilingProbability > 0.5){
+        setEmoji(smilingImg);
+      } else if(face.leftEyeOpenProbability < 0.5 && face.rightEyeOpenProbability > 0.5) {
+        setEmoji(winkingImg);
+      } else {
+        setEmoji(neutralImg);
+      }
+    }else {
+      setFaceDetected(false);
+    }
+  }
+  
   return (
     <View style={styles.container}>
-     
+      {faceDetected && <Animated.Image style={animatedStyle} source={emoji}/>}
+      <Camera 
+        style={styles.camera} 
+        type={CameraType.front}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.all,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}
+      />
     </View>
   );
 }
